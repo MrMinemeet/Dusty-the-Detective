eabbab869523d67eb127d45270e58489264e0312
Alexander Voglsperger <alexander.voglsperger+github@proton.me>
2023-10-27 13:20:05 +0200

    Add camera Move to Player and Adjust script
    MoveCamera script takes the MainCamera if not set explicitly

--- Assets/PlayerControl/MoveCamOverPlayer.cs
+++ Assets/PlayerControl/MoveCamOverPlayer.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class MoveCamOverPlayer : MonoBehaviour
 {
-    public Camera mainCamera;
+    [Tooltip("If the camera is not set, the main camera will be used.")]
+    public Camera playerCam;
     
     // TODO: Make borders that the camera won't move over
+    void Start() {
+        // If no camera is set, use the main camera by default
+        if (playerCam == null)
+        {
+            playerCam = Camera.main;
+        }
+    }
 
     // Update is called once per frame
     void Update()
     {
         // Move Camera over player with X and Y
         var position = transform.position;
-        mainCamera.transform.position = new Vector3(position.x, position.y, mainCamera.transform.position.z);
+        playerCam.transform.position = new Vector3(position.x, position.y, playerCam.transform.position.z);
     }
 }
