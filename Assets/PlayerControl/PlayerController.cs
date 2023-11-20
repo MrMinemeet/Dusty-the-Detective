f9c76abfd4f4f65f78c31e07a4786c4330568b00
rechenwerk-a <adrian.vinojcic@gmail.com>
2023-11-20 14:02:54 +0100

    Animate Mr Proper

--- Assets/PlayerControl/PlayerController.cs
+++ Assets/PlayerControl/PlayerController.cs
@@ -5,14 +5,15 @@ public class PlayerController : MonoBehaviour
 {
 	public float moveSpeed = 1f;
 
-
 	private Vector2 _moveInput = Vector2.zero;
 	private Rigidbody2D _rb;
+	private Animator _a;
 
 	// Start is called before the first frame update
-	private void Start()
+	void Start()
 	{
 		_rb = GetComponent<Rigidbody2D>();
+		_a = GetComponent<Animator>();
 	}
 
 	// Update is called once per frame
@@ -20,9 +21,19 @@ public class PlayerController : MonoBehaviour
 	{
 		_rb.velocity = _moveInput * moveSpeed;
 	}
-
-	private void OnMove(InputValue value)
+	void OnMove(InputValue value)
 	{
 		_moveInput = value.Get<Vector2>();
+		if (_moveInput.x != 0 || _moveInput.y != 0)
+		{
+			_a.SetFloat("X", _moveInput.x);
+			_a.SetFloat("Y", _moveInput.y);
+            
+			_a.SetBool("IsWalking", true);            
+		}
+		else
+		{
+			_a.SetBool("IsWalking", false);
+		}
 	}
 }
