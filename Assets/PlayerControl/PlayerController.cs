24742b4ea6e9dbba5c9d222caf6c09ac5573c55a
Rechenwerk | Adrian <64403586+rechen-werk@users.noreply.github.com>
2023-12-03 16:12:06 +0100

    Scene Change (#16)
    Scene Change with safe of the player position has been implemented. The only issue is that the start position of the player when entering a scene for the first time is set wrong.
    
    Co-authored-by: Flippochini <okicfilip@gmail.com>

--- Assets/PlayerControl/PlayerController.cs
+++ Assets/PlayerControl/PlayerController.cs
@@ -6,11 +6,13 @@ public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 3f;
     public Boolean savePosition = false;
+    public string gameManagerName;
 
     private Vector2 _moveInput = Vector2.zero;
     private Rigidbody2D _rb;
     private Animator _a;
     private GameManager _gameManager;
+    private bool initialSetupDone = true;
     
     // Start is called before the first frame update
     void Start()
@@ -19,12 +21,36 @@ public class PlayerController : MonoBehaviour
         _a = GetComponent<Animator>();
         if (savePosition)
         {
-            _gameManager = GameObject.FindWithTag("GameManager").GetComponent<GameManager>();
-            Vector3 newPosition = _gameManager.playerPosition;
-            float yOffset = 0.5f;
-            newPosition.y -= yOffset;
-            transform.position = newPosition;
+            _gameManager = GameObject.FindWithTag(gameManagerName).GetComponent<GameManager>();
+
+            if (!initialSetupDone)
+            {
+                SetInitialPlayerPosition();
+                initialSetupDone = true;
+            }
+            else
+            {
+                Vector3 newPosition = _gameManager.playerPosition;
+                float yOffset = 0.5f;
+                newPosition.y -= yOffset;
+                transform.position = newPosition; 
+            }
         }
+    }
+    
+    private void SetInitialPlayerPosition()
+    {
+        Vector3 initialPosition = new Vector3(0.0f, 0.0f, 0.0f);
+        
+        // Set the initial position as needed
+        if (gameManagerName == "GameManagerLobby")
+        {
+            initialPosition = new Vector3(-1.0f, -2.5f, 0.0f);
+        }
+
+        // Assign the initial position to the GameManager and the player
+        _gameManager.playerPosition = initialPosition;
+        transform.position = initialPosition;
     }
 
     // Update is called once per frame
