9b59d64d6147c49cec6f0ccfe1bb39e1f57b0841
Alexander Voglsperger <alexander.voglsperger+github@proton.me>
2023-12-03 20:34:52 +0100

    Make dirtspots cleanable
    Player can press 'E' when near a dirt spot

--- Assets/CleaningTask.cs
+++ Assets/CleaningTask.cs
@@ -2,11 +2,30 @@ using UnityEngine;
 
 public class CleaningTask : MonoBehaviour
 {
-	private void OnTriggerStay2D(Collider2D other)
+	private bool _canClean = false;
+
+	private void OnTriggerEnter2D(Collider2D other)
 	{
 		if (other.CompareTag("Player"))
 		{
-			Debug.Log("Player is in the cleaning area");
+			_canClean = true;
+		}
+	}
+
+	private void OnTriggerExit2D(Collider2D other)
+	{
+		if (other.CompareTag("Player"))
+		{
+			_canClean = false;
+		}
+	}
+
+	private void Update()
+	{
+		if (_canClean && Input.GetKeyDown(KeyCode.E))
+		{
+			Debug.Log("Player cleaned spot '" + gameObject.name + "'");
+			Destroy(gameObject);
 		}
 	}
 }
