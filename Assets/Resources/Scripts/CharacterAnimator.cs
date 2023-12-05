e33cd51286b9f15b1d1961b72262d4ebb63ac239
Rechenwerk | Adrian <64403586+rechen-werk@users.noreply.github.com>
2023-12-05 11:43:38 +0100

    remove automatic animation script and add prefabs for characters (#30)

---
+++ Assets/Resources/Scripts/CharacterAnimator.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class CharacterAnimator : MonoBehaviour
+{
+    private Rigidbody2D _rigidBody;
+    private Animator _animator;
+    void Start()
+    {
+        _rigidBody = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
+    }
+    
+    void Update()
+    {
+        Vector2 movement_vector = _rigidBody.velocity;
+        if (movement_vector.x != 0 || movement_vector.y != 0)
+        {
+            _animator.SetFloat("X", movement_vector.x);
+            _animator.SetFloat("Y", movement_vector.y);
+            
+            _animator.SetBool("IsWalking", true);            
+        }
+        else
+        {
+            _animator.SetBool("IsWalking", false);
+        }
+    }
+}
