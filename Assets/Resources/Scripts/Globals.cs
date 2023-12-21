87ccab341bcfe9ed78b2bb503f1e496b9fe03d2d
Alexander Voglsperger <alexander.voglsperger+github@proton.me>
2023-12-21 11:30:41 +0100

    Fix for placing dirt spot on different positions every time
    Instead of the position(vec3), the transform was stored, which caused it to be null (or to be precise to throw an error), when the floor was loaded again.
    When storing the position as vec3 that is not an issue anymore and they stay at the same spot.

--- Assets/Resources/Scripts/Globals.cs
+++ Assets/Resources/Scripts/Globals.cs
@@ -18,7 +18,7 @@ public static class Globals
 	}.AsReadOnly();
 
 	// Holds the position for each dirt spot on each the floors
-	public static readonly Dictionary<String, List<Transform>> TrashPositionMap = new();
+	public static readonly Dictionary<String, List<Vector3>> TrashPositionMap = new();
 
 	// Holds the sprite for each dirt spot on each the floors
 	public static readonly Dictionary<String, List<Sprite>> TrashSpriteMap = new();
@@ -41,11 +41,11 @@ public static class Globals
 		foreach (var floor in Floors)
 		{
 			List<Sprite> trashSpriteList = new List<Sprite>();
-			List<Transform> trashPositionList = new List<Transform>();
+			List<Vector3> trashPositionList = new List<Vector3>();
 			for (int i = 0; i < MAX_TRASH_PER_FLOOR; i++)
 			{
 				trashSpriteList.Add(sprites[UnityEngine.Random.Range(0, sprites.Length)]);
-				trashPositionList.Add(null);
+				trashPositionList.Add(Vector3.negativeInfinity); // Vector3 can't be null, so use negative infinity instead
 			}
 			TrashSpriteMap.Add(floor, trashSpriteList);
 			TrashPositionMap.Add(floor, trashPositionList);
