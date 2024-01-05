284b3d6f9ea7600b7e97cee20b1960188d02c555
Alexander Voglsperger <alexander.voglsperger+github@proton.me>
2024-01-05 09:06:05 +0100

    Add room signs to hallway
    Displays a text bubble that currently contains the name "hard"-painted into them.
    When coming closer to the door, it is shown. Otherwise it remains hidden.
    
    Kept it enabled per default and set do disabled in Start() in order to see them in editor

---
+++ Assets/Resources/Scripts/RoomSign.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomSign : MonoBehaviour
+{
+
+    private GameObject _nameBubble;
+
+    private void Awake()
+    {
+        _nameBubble = transform.Find("NameBubble").gameObject;
+    }
+    
+    private void Start()
+    {
+        // Disable name bubble per default
+        _nameBubble.gameObject.SetActive(false);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            _nameBubble.gameObject.SetActive(true);
+        }
+    }
+    
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") )
+        {
+            _nameBubble.gameObject.SetActive(false);
+        }
+    }
+}
