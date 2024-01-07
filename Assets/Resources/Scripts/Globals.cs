86cc6db51027999da4c0f614749afade93b0f171
Alexander Voglsperger <alexander.voglsperger+github@proton.me>
2024-01-07 17:09:05 +0100

    Limit trash to be Wine, glue, vomit
    This is to match the story

--- Assets/Resources/Scripts/Globals.cs
+++ Assets/Resources/Scripts/Globals.cs
@@ -34,7 +34,8 @@ public static class Globals
 		{ "Teacher", true }
 	};
 
-	private const int MAX_TRASH_PER_FLOOR = 2;
+	private const int MAX_TRASH_PER_FLOOR = 1;
+	private static readonly string[] TrashToUse = { "wine", "glue", "vomit" };
 	public static int CurrentFloor => Floors.IndexOf(CurrentFloorName);
 	public static string CurrentFloorName => SceneManager.GetActiveScene().name;
 
@@ -54,20 +55,24 @@ public static class Globals
 		#region TrashCreation
 		Debug.Log("Initializing Trash List...");
 
-		// Load all dirt spot sprites (using a "Object[]", as casting to "Sprite[]" causes it to be null, casting individual elements works though)
+		// Load all dirt spot sprites
 		Sprite[] sprites = Resources.LoadAll<Sprite>("DirtSpots/dirt_spots");
-		if (sprites == null)
+		sprites = sprites
+			.Where(s => TrashToUse.Any(name => s.name.Contains(name))).ToArray();
+		
+		if (sprites.Length == 0)
 		{
-			Debug.LogError("Could not load dirt spot sprites");
+			Debug.LogError("Could not load/select dirt spot sprites");
 			return;
 		}
 
+		int trashCounter = 0;
 		foreach (var floor in Floors)
 		{
 			List<Trash> trashList = new();
 			for (int i = 0; i < MAX_TRASH_PER_FLOOR; i++)
 			{
-				trashList.Add(new Trash(Vector3.negativeInfinity, sprites[Random.Range(0, sprites.Length)]));
+				trashList.Add(new Trash(Vector3.negativeInfinity, sprites[trashCounter++]));
 			}
 			trashMap.Add(floor, trashList);
 		}
