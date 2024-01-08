e064d26ceed542c7562c6e5e6b2b643606c6870c
Alexander Voglsperger <alexander.voglsperger+github@proton.me>
2024-01-08 18:10:20 +0100

    Add DontPressTheButton Minigame

--- Assets/Resources/Scripts/DontPressTheButton.cs
+++ Assets/Resources/Scripts/DontPressTheButton.cs
@@ -1,45 +1,63 @@
+using System;
 using TMPro;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 public class DontPressTheButton : MonoBehaviour
 {
-    private const float TIME_TO_NOT_PRESS = 10f;
-    private float _timeShown;
-    
-    public TextMeshProUGUI timeLeftText;
-    
-    private void Update()
-    {
-        _timeShown += Time.deltaTime;
-        
-        // Update time left
-        float timeLeft = TIME_TO_NOT_PRESS - _timeShown;
-        timeLeftText.text = $"{timeLeft:F1} seconds left";
-        
-        
-        if (_timeShown >= TIME_TO_NOT_PRESS)
-        {
-            // Trigger successful cleaning
-            
-            Disable();
-        }
-    }
+	private const float DEFAULT_TIME_TO_WAIT = 10f;
 
-    public void OnPressed()
-    {
-        if (!(_timeShown < TIME_TO_NOT_PRESS)) return;
-        Debug.Log("The button that should not be pressed, was pressed!");
-        EasterEgg.OpenRR();
-            
-        Disable();
-    }
+	public TextMeshProUGUI timeLeftText;
+	public Animator animator;
+	private bool _hasBeenPlayed;
+	private float _timeShown;
 
-    /**
-     * Disables the minigame
-     */
-    private void Disable()
-    {
-        transform.gameObject.SetActive(false);
-    }
+	private float _timeToWait;
+
+	private void Update()
+	{
+		if (!_hasBeenPlayed && Globals.LeftoverTrash == 1)
+			StartMinigame();
+		else if (Globals.LeftoverTrash != 1 || !_hasBeenPlayed)
+			return;
+
+
+		_timeShown += Time.deltaTime;
+
+		// Update time left
+		var timeLeft = _timeToWait - _timeShown;
+		timeLeftText.text = $"{timeLeft:F1} seconds left";
+
+
+		if (_timeShown >= _timeToWait)
+			// Trigger successful cleaning
+			Disable();
+	}
+
+	public void OnPressed()
+	{
+		if (!(_timeShown < _timeToWait)) return; // Time was already over and button should hide
+		
+		Debug.Log("The button that should not be pressed, was pressed!");
+		EasterEgg.OpenRR();
+
+		_timeToWait += 5f;
+	}
+
+	/**
+	 * Disables the minigame
+	 */
+	private void Disable()
+	{
+		Debug.Log("Disabling DontPressTheButton minigame");
+		animator.Play("hide");
+		Destroy(this);
+	}
+
+	private void StartMinigame()
+	{
+		Debug.Log("Starting DontPressTheButton minigame");
+		animator.Play("show");
+		_timeToWait = DEFAULT_TIME_TO_WAIT;
+		_hasBeenPlayed = true;
+	}
 }
