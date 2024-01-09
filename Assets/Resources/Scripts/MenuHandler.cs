MrMinemeet/Dusty-the-Detective

commit d252771c93a7ce82e96f489da08927c5a6af5ca3

(Re-)Set globals when loading from the main menu
Otherwise all flags and values stay the  same

Assets/Resources/Scripts/MenuHandler.cs
@@ -4,6 +4,9 @@ public class MenuHandler : MonoBehaviour
 {
     public void PlayGame()
     {
+        // Reset globals
+        Globals.ResetGlobals();
+        
         // Load main menu
         StartCoroutine(FindObjectOfType<LevelLoader>().LoadLevel("Lobby"));
     }

Assets/Resources/Scripts/CleaningTask.cs
@@ -36,9 +36,9 @@ public class CleaningTask : MonoBehaviour
         Debug.Log(trash.Image.name);
 		switch(trash.Image.name)
 		{
-			case "dirt_spot_wine": Globals.wineStatus = Globals.TrashStatus.COLLECTED; break;
-			case "dirt_spot_vomit": Globals.vomitStatus = Globals.TrashStatus.COLLECTED; break;
-			case "dirt_spot_glue": Globals.glueStatus = Globals.TrashStatus.COLLECTED; break;
+			case "dirt_spot_wine": Globals.wineStatus = TrashStatus.COLLECTED; break;
+			case "dirt_spot_vomit": Globals.vomitStatus = TrashStatus.COLLECTED; break;
+			case "dirt_spot_glue": Globals.glueStatus = TrashStatus.COLLECTED; break;
 		}
 
         Globals.trashMap[Globals.CurrentFloorName].Remove(trash);

Assets/Resources/Scripts/Globals.cs
@@ -11,25 +11,29 @@ public static class Globals
 	// Times in seconds for guests range of leaving
 	public const int MIN_TIME_UNTIL_LEAVE = 10 * 60;
 	public const int MAX_TIME_UNTIL_LEAVE = 20 * 60;
-
-	// Variables to store whether a dirt spot is cleaned and disposed
-	public enum TrashStatus
-	{
-		ACTIVE, COLLECTED, DISPOSED
-	}
-	public static TrashStatus vomitStatus = TrashStatus.ACTIVE;
-	public static TrashStatus wineStatus = TrashStatus.ACTIVE;
-	public static TrashStatus glueStatus = TrashStatus.ACTIVE;
+	private const int MAX_TRASH_PER_FLOOR = 1;
+	
+	public static TrashStatus vomitStatus;
+	public static TrashStatus wineStatus;
+	public static TrashStatus glueStatus;
 	
 	//bools for correct assuming which npc was guilty, used by DialogueManager
-	public static bool vomitCorrect = false;
-	public static bool spilledWineCorrect = false;
-	public static bool glueCorrect = false;
+	public static bool vomitCorrect;
+	public static bool spilledWineCorrect;
+	public static bool glueCorrect;
 	//bool for animating the correct guilt dialogue
-	public static bool showGuiltDialogue = false;
+	public static bool showGuiltDialogue;
 
 	// Total time in seconds of the game running
-	public static double TotalTimeRunning = 0;
+	public static double TotalTimeRunning;
+
+	/**
+	 * Provides the amount of trash left in total
+	 */
+	public static int LeftoverTrash => trashMap.Values.SelectMany(trash => trash).Count();
+	public static int CurrentFloor => Floors.IndexOf(CurrentFloorName);
+	public static string CurrentFloorName => SceneManager.GetActiveScene().name;
+	
 
 	public static readonly Dictionary<string, float> TimeUntilGuestLeaves = new()
 	{
@@ -50,10 +54,7 @@ public static class Globals
 		{ "Teacher", true }
 	};
 
-	private const int MAX_TRASH_PER_FLOOR = 1;
 	private static readonly string[] TrashToUse = { "wine", "glue", "vomit" };
-	public static int CurrentFloor => Floors.IndexOf(CurrentFloorName);
-	public static string CurrentFloorName => SceneManager.GetActiveScene().name;
 
 	public static readonly ReadOnlyCollection<string> Floors = new List<string>
 	{
@@ -94,11 +95,30 @@ public static class Globals
 		}
 		#endregion
 	}
+	
+	public static void ResetGlobals()
+	{
+		// Trash status
+		vomitStatus = TrashStatus.ACTIVE;
+		wineStatus = TrashStatus.ACTIVE;
+		glueStatus = TrashStatus.ACTIVE;
+		
+		// Flags
+		vomitCorrect = false;
+		spilledWineCorrect = false;
+		glueCorrect = false;
+		showGuiltDialogue = false;
+		
+		// Other values
+		TotalTimeRunning = 0f;
 
-	/**
-	 * Provides the amount of trash left in total
-	 */
-	public static int LeftoverTrash => trashMap.Values.SelectMany(trash => trash).Count();
+		foreach (string key in ActiveGuestMap.Keys)
+			ActiveGuestMap[key] = true;
+		
+		// Reset trash map
+		trashMap.Clear();
+		Initialize();
+	}
 }
 
 public record Trash
@@ -111,4 +131,11 @@ public record Trash
 		Position = position;
 		Image = image;
 	}
+}
+
+
+// Variables to store whether a dirt spot is cleaned and disposed
+public enum TrashStatus
+{
+	ACTIVE, COLLECTED, DISPOSED
 }

Assets/Resources/Scripts/TrashcanController.cs
@@ -48,17 +48,17 @@ public class TrashcanController : MonoBehaviour
 
     private void TriggerDialogue()
     {
-        if (Globals.glueStatus == Globals.TrashStatus.DISPOSED &&
-            Globals.vomitStatus == Globals.TrashStatus.DISPOSED &&
-            Globals.wineStatus == Globals.TrashStatus.DISPOSED)
+        if (Globals.glueStatus == TrashStatus.DISPOSED &&
+            Globals.vomitStatus == TrashStatus.DISPOSED &&
+            Globals.wineStatus == TrashStatus.DISPOSED)
         {
             DialogueManager.Instance.StartDialogue(allCollectedDialogue, audioSource);
             return;
         }
 
-        if (Globals.glueStatus != Globals.TrashStatus.COLLECTED && 
-            Globals.vomitStatus != Globals.TrashStatus.COLLECTED && 
-            Globals.wineStatus != Globals.TrashStatus.COLLECTED)
+        if (Globals.glueStatus != TrashStatus.COLLECTED && 
+            Globals.vomitStatus != TrashStatus.COLLECTED && 
+            Globals.wineStatus != TrashStatus.COLLECTED)
         {
             DialogueManager.Instance.StartDialogue(greetingDialogue, audioSource);
             return;
@@ -66,25 +66,25 @@ public class TrashcanController : MonoBehaviour
 
         var combinedDialogue = new List<Dialogue>();
 
-        if(Globals.glueStatus == Globals.TrashStatus.COLLECTED)
+        if(Globals.glueStatus == TrashStatus.COLLECTED)
         {
             combinedDialogue.Add(hasGlueDialogue);
-            Globals.glueStatus = Globals.TrashStatus.DISPOSED;
+            Globals.glueStatus = TrashStatus.DISPOSED;
         }
-        if (Globals.vomitStatus == Globals.TrashStatus.COLLECTED)
+        if (Globals.vomitStatus == TrashStatus.COLLECTED)
         {
             combinedDialogue.Add(hasVomitDialogue);
-            Globals.vomitStatus = Globals.TrashStatus.DISPOSED;
+            Globals.vomitStatus = TrashStatus.DISPOSED;
         }
-        if (Globals.wineStatus == Globals.TrashStatus.COLLECTED)
+        if (Globals.wineStatus == TrashStatus.COLLECTED)
         {
             combinedDialogue.Add(hasWineDialogue);
-            Globals.wineStatus = Globals.TrashStatus.DISPOSED;
+            Globals.wineStatus = TrashStatus.DISPOSED;
         }
 
-        if (Globals.glueStatus == Globals.TrashStatus.DISPOSED &&
-            Globals.vomitStatus == Globals.TrashStatus.DISPOSED &&
-            Globals.wineStatus == Globals.TrashStatus.DISPOSED)
+        if (Globals.glueStatus == TrashStatus.DISPOSED &&
+            Globals.vomitStatus == TrashStatus.DISPOSED &&
+            Globals.wineStatus == TrashStatus.DISPOSED)
         {
             combinedDialogue.Add(allCollectedDialogue);
         }

commit 6a27c8644c369311f91a6d1d40131e8ed4b38d6b

Deactivate reset globals
The reset of globals from commit d252771c93a7ce82e96f489da08927c5a6af5ca3 caused some issues. Deactivated the method call for now

Assets/Resources/Scripts/MenuHandler.cs
@@ -5,7 +5,8 @@ public class MenuHandler : MonoBehaviour
     public void PlayGame()
     {
         // Reset globals
-        Globals.ResetGlobals();
+        // FIXME: This causes some issues
+        //Globals.ResetGlobals();
         
         // Load main menu
         StartCoroutine(FindObjectOfType<LevelLoader>().LoadLevel("Lobby"));