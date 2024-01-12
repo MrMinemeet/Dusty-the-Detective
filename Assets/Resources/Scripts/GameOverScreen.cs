13a82239422b4dde5fda35239b5d753095dab91b
Alexander Voglsperger <alexander.voglsperger+github@proton.me>
2024-01-12 19:10:28 +0100

    Make Pause Menu unavailable in Game Over Screen
    Instead add a "Main Menu" button

--- Assets/Resources/Scripts/GameOverScreen.cs
+++ Assets/Resources/Scripts/GameOverScreen.cs
@@ -3,15 +3,24 @@ using UnityEngine;
 
 public class GameOverScreen : MonoBehaviour
 {
+    public static bool IsActive;
+    
     public TextMeshProUGUI vomitResult;
     public TextMeshProUGUI wineResult;
     public TextMeshProUGUI glueResult;
     
     public void Setup(bool vomit, bool wine, bool glue)
     {
+        IsActive = true;
         gameObject.SetActive(true);
         vomitResult.text = vomit ? "CORRECT" : "INCORRECT";
         wineResult.text = wine ? "CORRECT" : "INCORRECT";
         glueResult.text = glue ? "CORRECT" : "INCORRECT";
+    }
+
+    public void OnMainMenuClick()
+    {
+        // Load main menu
+        StartCoroutine(FindObjectOfType<LevelLoader>().LoadLevel("MainScreen"));
     }
 }
